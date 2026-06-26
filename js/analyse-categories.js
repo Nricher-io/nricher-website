fc9f641c0955cd25d7007ac8885cee5d1081d402
Quentin Malabave <qmfire18@gmail.com>
2026-06-26 15:02:19 +0200

    fix: retire les 73 enseignes sans donnees Weekly KPIs des categories d'analyse, supprime 2 categories devenues vides (Sante & Pharmacie, Mode & Luxe)

--- js/analyse-categories.js
+++ js/analyse-categories.js
@@ -10,47 +10,37 @@
     {
       id: 'maison-deco',
       name: 'Maison & Décoration',
-      brands: ['ambientedirect', 'Andlight', 'batinea', 'Bestmobilier', 'Bifor', 'chiaracolombini', 'connox', 'design-bestseller', 'drawer', 'fermliving', 'finnishdesignshop', 'flos', 'Galerieslafayette', 'gautier', 'grouperg', 'Inside_75', 'Jardindeco', 'Lacompagniedulit', 'lamptwist', 'lecedrerouge', 'light11', 'Lightonline', 'Madeindesign', 'madeindesign de', 'madeindesignuk', 'Maisons du Monde', 'maisons du monde es', 'maisons du monde it', 'Manelli', 'maxibazar', 'mabeoindustries', 'myareadesign', 'Nedgis', 'Neuvieme_store', 'Nouveau marchand', 'onelec', 'Passions Cadeaux', 'red deco', 'Silvera', 'Unamourdetapis', 'Vente Unique', 'vente unique de', 'vente unique es', 'vente unique it', 'Weber', 'westwing', 'Manor', 'Printemps', '3Suisses']
+      brands: ['Andlight', 'chiaracolombini', 'connox', 'design-bestseller', 'drawer', 'fermliving', 'gautier', 'grouperg', 'Jardindeco', 'lamptwist', 'lecedrerouge', 'light11', 'Madeindesign', 'madeindesign de', 'madeindesignuk', 'Maisons du Monde', 'maisons du monde es', 'maisons du monde it', 'maxibazar', 'myareadesign', 'Nedgis', 'Silvera', 'Vente Unique', 'vente unique de', 'vente unique es', 'vente unique it']
     },
     {
       id: 'bricolage-jardin',
       name: 'Bricolage & Jardin',
-      brands: ['Bricomarche', 'Castorama', 'leroy merlin', 'leroy merlin es', 'leroy merlin it', 'Manomano', 'manomano es', 'manomano it', 'Hydrozone']
+      brands: ['Castorama', 'leroy merlin', 'Manomano', 'manomano it']
     },
     {
       id: 'sport-outdoor',
       name: 'Sport & Outdoor',
-      brands: ['alltricks', 'alpiniste', 'ekosport', 'glisshop', 'hardloop', 'Icasque', 'irun', 'skapetze', 'Snowleader', 'Snowleader CH', 'sportdecouverte', 'trekkinn', 'centrale du casque']
+      brands: ['alltricks', 'alpiniste', 'ekosport', 'glisshop', 'hardloop', 'irun', 'Snowleader', 'Snowleader CH', 'trekkinn']
     },
     {
       id: 'beaute-parfumerie',
       name: 'Beauté & Parfumerie',
-      brands: ['aurlane', 'Boticinal', 'Clarins', 'Lancome', 'LOreal', 'Payot', 'sephora', 'Sisley', 'SVR']
-    },
-    {
-      id: 'sante-pharmacie',
-      name: 'Santé & Pharmacie',
-      brands: ['Atida', 'Cocooncenter', 'Easyparapharmacie', 'Pharmazon']
-    },
-    {
-      id: 'mode-luxe',
-      name: 'Mode & Luxe',
-      brands: ['bulgari', 'Hermes', 'JPG', 'Sandro', 'Sandro_BDV', 'SenNoSen', 'debenhams']
+      brands: ['aurlane', 'sephora']
     },
     {
       id: 'electronique-hightech',
       name: 'Électronique & High-Tech',
-      brands: ['Boulanger', 'BUT', 'Darty', 'Fnac', 'Pixmania', 'Ubaldi', 'bq']
+      brands: ['Boulanger', 'BUT', 'Darty', 'Fnac', 'bq']
     },
     {
       id: 'mobilite',
       name: 'Mobilité (Auto, Moto, Vélo)',
-      brands: ['Dafymoto', 'Louis Moto', 'Motoaxxe', 'Motoblouz', 'Motocard', 'Teamaxe', 'Voltex', 'WTB', 'speedway', 'Mobin']
+      brands: ['Mobin']
     },
     {
       id: 'bebe-puericulture',
       name: 'Bébé & Puériculture',
-      brands: ['berceaumagique', 'madeinbebe', 'Petit Bateau', 'vertbaudet']
+      brands: ['berceaumagique']
     },
     {
       id: 'grande-distribution',
@@ -60,12 +50,12 @@
     {
       id: 'marketplaces',
       name: 'Marketplaces en ligne',
-      brands: ['Amazon', 'amazon es', 'amazon it', 'Rakuten', 'CDiscount', 'Rue du commerce', 'shopping_feed']
+      brands: ['Amazon', 'amazon es', 'amazon it', 'CDiscount', 'Rue du commerce']
     },
     {
       id: 'culture-loisirs',
       name: 'Culture & Loisirs',
-      brands: ['cultura', 'viapresse', 'Belong', 'Bewak', 'Labecanerie', 'Maxxess', 'Thecoolrepublic', 'Underdog', 'zoomici', 'Asteri', 'Simplybearings']
+      brands: ['Thecoolrepublic', 'Underdog', 'Asteri', 'Simplybearings']
     }
   ];
 })();
